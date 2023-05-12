6933b270cf33a41ea7a337aec26bd7f8c1694621
JohannSiemens <johann.siemens85@gmx.de>
2023-05-12 10:29:58 +0200

    first commit, final app version

--- components/Product/index.js
+++ components/Product/index.js
@@ -2,13 +2,48 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
-import Comments from "../../../../backend-create/products/components/Comments";
+//Alter Import ist kaputt: import Comments from "../../../../backend-create/products/components/Comments";
+import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+import { StyledButton } from "../Button/Button.styled";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.log(response.status);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -26,6 +61,19 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <StyledButton
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        {isEditMode ? "Hide edit form" : "Show edit form"}
+      </StyledButton>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct} isEdit={isEditMode} />
+      )}
+      <StyledButton onClick={() => handleDeleteProduct(id)}>
+        Delete product
+      </StyledButton>
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -13,5 +13,30 @@ export default async function handler(request, response) {
     }
 
     response.status(200).json(product);
+    return;
+  }
+
+  if (request.method === "PUT") {
+    const product = await Product.findByIdAndUpdate(id, { $set: request.body });
+
+    if (!product) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+
+    response.status(200).json({ status: "Product successfully updated." });
+    return;
+  }
+
+  if (request.method === "DELETE") {
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+
+    response.status(200).json({ status: "Product successfully deleted." });
+    return;
   }
 }
